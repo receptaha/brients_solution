19585f316ed57f6d4491030c58d0c225fdc0b2cc
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-11-26 16:55:20 +0300

    main.c updated.

--- main.c
+++ main.c
@@ -7,6 +7,32 @@
 int main()
 {
     AddressSpace* as = init_address_space();
-    free(as);
+    Person* person1 = malloc(sizeof(Person));
+    person1->id = 3;
+    person1->age = 25;
+    person1->name = "Recep Taha Ayvaz";
+    force_insert(as, person1);
+
+    Person* person2 = malloc(sizeof(Person));
+    person2->id = 4;
+    person2->age = 21;
+    person2->name = "Cihan Şahin";
+    force_insert(as, person2);
+
+    Person* person3 = malloc(sizeof(Person));
+    person3->id = 5;
+    person3->age = 25;
+    person3->name = "Alper Yıldırım";
+    force_insert(as, person3);
+
+    Person* person4 = malloc(sizeof(Person));
+    person4->id = 8;
+    person4->age = 25;
+    person4->name = "Ilgın Geçgel";
+    force_insert(as, person4);
+
+    print_hash_table(as);
+
+    get(as, 8);
     return 0;
 }
