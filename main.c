d17c68519d02e609070b3fe3553504d8a89da11c
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-11-27 20:48:46 +0300

    main.c updated

--- main.c
+++ main.c
@@ -27,12 +27,31 @@ int main()
 
     Person* person4 = malloc(sizeof(Person));
     person4->id = 8;
-    person4->age = 25;
-    person4->name = "Ilgın Geçgel";
+    person4->age = 21;
+    person4->name = "Mehmet X1";
     force_insert(as, person4);
+
+    Person* person5 = malloc(sizeof(Person));
+    person5->id = 15;
+    person5->age = 21;
+    person5->name = "Mehmet X2";
+    force_insert(as, person5);
+
+    Person* person6 = malloc(sizeof(Person));
+    person6->id = 19;
+    person6->age = 21;
+    person6->name = "Mehmet X3";
+    force_insert(as, person6);
+
+    Person* person7 = malloc(sizeof(Person));
+    person7->id = 12376;
+    person7->age = 21;
+    person7->name = "Mehmet X4";
+    force_insert(as, person7);
 
     print_hash_table(as);
 
-    get(as, 8);
+    get(as, 129387123);
+
     return 0;
 }
